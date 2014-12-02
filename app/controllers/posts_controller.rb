e62a6f9b97a1bde17d8f5f17dbec876b0590bab7
Robert Lee Brackett III <robert.brackett3@gmail.com>
2014-12-02 14:00:25 -0500

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,18 +6,24 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    # @user = User.find(params[:user_id])
+    # @posts = @user.posts.all
   end
 
   def new 
-    @post = User.where(params[:id]).post.new
+    @post = User.where(params[:id]).post.new # This should read as params[:user_id]
   end
 
   def edit
     @user = User.post.find(params[:id])
+    # @user = User.find(params[:user_id])
+    # @post = @user.posts.find(params[:id])
   end
 
   def show
     @user = User.post.find(params[:id])
+    # @user = User.find(params[:user_id])
+    # @post = @user.posts.find(params[:id])
   end
 
   ######################################
@@ -26,15 +32,22 @@ class PostsController < ApplicationController
 
   def create
     @post = User.post.create
+    # @user = User.find(params[:user_id])
+    # @post = @user.posts.create
   end
 
   def update
     @user = User.find(params[:id])
+    # @user = User.find(params[:user_id])
+    # @post = @user.posts.find(params[:id])
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    # @user = User.find(params[:user_id])
+    # @post = @user.posts.find(params[:id])
+    # @post.destroy
   end
 
 end
